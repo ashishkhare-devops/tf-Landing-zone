a11f0c0f6da1b6af6ab7201120a4b725d538a2b7
Ashish Khare <ashishkhare.0706@gail.com>
2026-08-20 04:11:47 +0530

    Fix terraform validate & container dependency issue

--- module/azurerm_storage_container/main.tf
+++ module/azurerm_storage_container/main.tf
@@ -1,7 +1,6 @@
 resource "azurerm_storage_container" "cnt" {
-    for_each = var.ctn
-    name                  = each.value.name
-   container_access_type = each.value.container_access_type
-   storage_account_name  = each.value.storage_account_name
-  
+  for_each              = var.ctn
+  name                  = each.value.name
+  container_access_type = each.value.container_access_type
+  storage_account_id    = data.azurerm_storage_account.sa[each.key].id
 }
