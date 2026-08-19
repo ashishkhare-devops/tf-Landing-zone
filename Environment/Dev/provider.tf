d9a7e88e3009ed4c5040941fa7e4b8c703675da3
Ashish Khare <ashishkhare.0706@gail.com>
2026-08-20 01:20:25 +0530

    added backend block

--- Environment/Dev/provider.tf
+++ Environment/Dev/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "5.0.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "dev-rg1"
+    storage_account_name = "devstorageaccountsa1"
+    container_name       = "devcontainer1"
+    key                  = "dev.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
